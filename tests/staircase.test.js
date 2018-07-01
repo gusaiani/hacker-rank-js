09492bdb9273ba90b86a625d35f30f8217670b11
Gustavo Saiani <gustavo@poe.ma>
2018-07-01 20:00:42 -0300

    Solve Staircase

---
+++ tests/staircase.test.js
@@ -0,0 +1,9 @@
+/*
+ * https://www.hackerrank.com/challenges/staircase/problem
+ */
+
+const staircase = require('../staircase');
+
+test('A staircase of 6', () => {
+  expect(staircase(6)).toEqual("     #\n    ##\n   ###\n  ####\n #####\n######\n");
+});
