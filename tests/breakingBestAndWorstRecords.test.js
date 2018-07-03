0ae6375f5a85fe99e86c0d2a5b92faee75f2c9ab
Gustavo Saiani <gustavo@poe.ma>
2018-07-03 11:05:32 -0300

    Solve breakingBestAndWorstRecords

---
+++ tests/breakingBestAndWorstRecords.test.js
@@ -0,0 +1,7 @@
+const breakingBestAndWorstRecords = require('../breakingBestAndWorstRecords');
+
+test('A breakingBestAndWorstRecords test', () => {
+  const scores = "10 5 20 20 4 5 2 25 1"
+  const arr = scores.split(" ").map(Number)
+  expect(breakingBestAndWorstRecords(arr)).toEqual('2 4');
+});
