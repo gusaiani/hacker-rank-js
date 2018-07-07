17e841218c8c9f6ecca51f34b7ba1527ec4fc4d9
Gustavo Saiani <gustavo@poe.ma>
2018-07-07 18:58:51 -0300

    Solve matchingSocks

---
+++ tests/matchingSocks.test.js
@@ -0,0 +1,9 @@
+/*
+ * https://www.hackerrank.com/challenges/sock-merchant/problem
+ */
+const matchingSocks = require('../matchingSocks');
+
+test('A matchingSocks test', () => {
+  const arr = "10 20 20 10 10 30 50 10 20".split(" ")
+  expect(matchingSocks(arr)).toEqual(3);
+});
