000bc6590ed8bee9c34e9ba304dbfb0d29f0cdc1
Gustavo Saiani <gustavo@poe.ma>
2018-07-10 18:47:35 -0300

    Solve catsAndMouse

---
+++ catsAndMouse.js
@@ -0,0 +1,10 @@
+function catsAndMouse(x, y, z) {
+  const AtoC = Math.abs(x - z)
+  const BtoC = Math.abs(y - z)
+
+  if (AtoC > BtoC) return 'Cat B'
+  else if (BtoC > AtoC) return 'Cat A'
+  return 'Mouse C'
+}
+
+module.exports = catsAndMouse;
---
+++ tests/catsAndMouse.test.js
@@ -0,0 +1,9 @@
+const catsAndMouse = require('../catsAndMouse');
+
+test('A catsAndMouse test', () => {
+  expect(catsAndMouse(1, 2, 3)).toEqual("Cat B");
+});
+
+test('A catsAndMouse test', () => {
+  expect(catsAndMouse(1, 3, 2)).toEqual("Mouse C");
+});
