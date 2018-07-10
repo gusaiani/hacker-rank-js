704ed2c4bd0788bda2534b6d854c74ad6b7feaa9
Gustavo Saiani <gustavo@poe.ma>
2018-07-10 12:56:36 -0300

    Solve ctciBubbleSort

---
+++ tests/ctciBubbleSort.test.js
@@ -0,0 +1,6 @@
+const ctciBubbleSort = require('../ctciBubbleSort');
+
+test('A ctciBubbleSort test', () => {
+  const arr = "1 2 3".split(" ").map(Number)
+  expect(ctciBubbleSort(arr)).toEqual("0 1 3");
+});
