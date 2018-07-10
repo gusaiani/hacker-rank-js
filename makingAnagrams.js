83fb4e2678492de319505031bc5e39d38d72ea61
Gustavo Saiani <gustavo@poe.ma>
2018-07-10 10:24:29 -0300

    Solve makingAnagrams

---
+++ makingAnagrams.js
@@ -0,0 +1,35 @@
+function makingAnagrams(str1, str2) {
+  let arr1 = makeSortedArray(str1)
+  let arr2 = makeSortedArray(str2)
+  let deletions = 0
+
+  while (arr1.length || arr2.length) {
+    if (arr1.length == 0) {
+      deletions += arr2.length;
+      break;
+    }
+    if (arr2.length == 0) {
+      deletions += arr1.length;
+      break;
+    }
+
+    if(arr1[0] == arr2[0]) {
+      arr1.shift()
+      arr2.shift()
+    } else if (arr1[0] < arr2[0]) {
+      deletions++
+      arr1.shift()
+    } else if (arr1[0] > arr2[0]) {
+      deletions++
+      arr2.shift()
+    }
+  }
+
+  return deletions;
+}
+
+function makeSortedArray(str) {
+  return str.split(" ").join('').split('').sort()
+}
+
+module.exports = makingAnagrams;
---
+++ tests/makingAnagrams.test.js
@@ -0,0 +1,5 @@
+const makingAnagrams = require('../makingAnagrams');
+
+test('A makingAnagrams test', () => {
+  expect(makingAnagrams("cde", "abc")).toEqual(4);
+});
