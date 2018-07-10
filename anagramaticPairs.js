8599d0cf61eef9614d0355b0792cb75dcdcc2f16
Gustavo Saiani <gustavo@poe.ma>
2018-07-10 18:02:15 -0300

    Solve anagramaticPairs

---
+++ anagramaticPairs.js
@@ -0,0 +1,39 @@
+function anagramaticPairs(arr) {
+  return arr.map(str => {
+    return countAnagramaticPairs(str)
+  })
+}
+
+function countAnagramaticPairs(str) {
+  const arr = str.split('')
+  let pairs = 0;
+
+  // Iterate from sizes of 1 to arr.length - 1
+  for (let s = 1; s < arr.length; s++) {
+
+    for(let i = 0; i <= arr.length - s; i++) {
+
+      const substr1 = arr.slice(i, i + s).sort().join('')
+
+      for(let j = i + 1; j <= arr.length - s; j++) {
+
+        const substr2 = arr.slice(j, j + s).sort().join('')
+
+        if (substr1 == substr2) pairs++
+      }
+
+    }
+
+  }
+
+  return pairs
+}
+
+function areAnagrams(arr1, arr2) {
+  arr1.sort()
+  arr2.sort()
+
+  return arr1.join('') == arr2.join('')
+}
+
+module.exports = anagramaticPairs;
