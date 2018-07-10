0a8bcaa5f8263cf7700fe193be20ae33088412f1
Gustavo Saiani <gustavo@poe.ma>
2018-07-09 21:26:17 -0300

    Solve countingValleys

---
+++ tests/countingValleys.test.js
@@ -0,0 +1,6 @@
+const countingValleys = require('../countingValleys');
+
+test('A countingValleys test', () => {
+  const arr = "UDDDUDUU".split("")
+  expect(countingValleys(arr.length, arr)).toEqual(1);
+});
