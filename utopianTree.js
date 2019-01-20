5581c7843fd00ec521e49b46f54fac1a48f258d1
Gustavo Saiani <gustavo@poe.ma>
2019-01-20 18:26:26 -0200

    Solve Utopian Tree

---
+++ utopianTree.js
@@ -0,0 +1,16 @@
+/*
+ * Solve https://www.hackerrank.com/challenges/utopian-tree/problem
+ */
+function utopianTree(cycles) {
+  let height = 1
+
+  for (let i = 1; i <= cycles; i++) {
+    height = (i % 2 === 0) ?
+      height + 1 :
+      height * 2
+  }
+
+  return height
+}
+
+module.exports = utopianTree;
