a7b7d9b4ec99622cda7efd1447e0dbc06683386e
Gustavo Saiani <gustavo@poe.ma>
2019-12-08 05:33:45 -0300

    Solve Sherlock valid string

---
+++ sherlockValidString.js
@@ -0,0 +1,69 @@
+// https://www.hackerrank.com/challenges/sherlock-and-valid-string/
+function sherlockValidString(str) {
+  return booleanSherlockValidString(str) ? 'YES' : 'NO'
+}
+
+function booleanSherlockValidString(str) {
+  let hashMap = {}
+  let uniqueCounts = []
+  for (let char of str) {
+    hashMap[char] = hashMap[char] ? hashMap[char] + 1 : 1
+  }
+
+  let countHashMap = {}
+
+  Object.keys(hashMap).forEach((char) => {
+    const num = hashMap[char]
+
+    if (!countHashMap[num]) {
+      countHashMap[num] = [char]
+    } else {
+      if (!countHashMap[num].includes(char)) {
+        countHashMap[num] = [...countHashMap[num], char]
+      }
+    }
+  })
+
+  const numberOfDifferentKeyCounts =
+    Object.keys(countHashMap).length
+
+  if (numberOfDifferentKeyCounts < 2) {
+    return true
+  }
+
+  if (numberOfDifferentKeyCounts > 2) {
+    return false
+  }
+
+  const countHashMapKeys = Object.keys(countHashMap)
+  let countsWithMultipleChars = 0
+
+  for (let i = 0; i < countHashMapKeys.length; i++) {
+    const count = countHashMapKeys[i]
+    const countInt = parseInt(count)
+
+    if (
+      (countInt === 1)
+      && (countHashMap[count].length === 1)
+    ) {
+      return true
+    }
+
+    if (countHashMap[count].length > 1) {
+      if (countsWithMultipleChars > 0) {
+        return false
+      }
+      countsWithMultipleChars++
+    }
+  }
+
+  const [a, b] = Object.keys(countHashMap)
+
+  if (Math.abs(a - b) != 1) return false
+
+  const largestCharCount = Math.max(a, b)
+
+  return countHashMap[largestCharCount].length === 1
+}
+
+module.exports = sherlockValidString
